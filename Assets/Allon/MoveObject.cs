b9060dbcf127cd7246335adf3988104f3ee6320f
MasterWoland <mark@allon.nl>
2019-12-22 12:54:41 +0100

    Second commit

---
+++ Assets/Allon/MoveObject.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    void Update()
+    {
+        float speed = 2f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * -speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * -speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * speed * 10f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * speed * -10f);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(Vector3.right, Time.deltaTime * speed * -10f);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(Vector3.right, Time.deltaTime * speed * 10f);
+        }
+    }
+}
---
+++ Assets/Allon/NOT NOW/NetworkSceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetworkSceneManager : NetworkBehaviour
+{
+   [Scene] public string ServerScene;
+   [Scene] public string ClientScene;
+   
+   public override void OnStartServer()
+   {
+      base.OnStartServer();
+
+      Debug.Log("We load the server scene");
+
+      SceneManager.LoadScene(ServerScene, LoadSceneMode.Single);
+   }
+
+   public override void OnStartClient()
+   {
+      base.OnStartClient();
+
+      Debug.Log("We load the CLIENT scene");
+      
+      SceneManager.LoadScene(ClientScene, LoadSceneMode.Single);
+   }
+}
